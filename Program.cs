7486ccc3156d06f626addff32e81de896c81b8ee
ivankoros <ivan.koros2@gmail.com>
2023-04-03 19:40:52 -0400

    added beginning to story, getting (valid) user name with regex and then escaping some ascii art with a verbatim string to print out a campfire

--- Program.cs
+++ Program.cs
@@ -1,3 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Threading;
+using System.Text.RegularExpressions;
 
-Console.WriteLine("Hello, World!");
+// This is an text-based adventure game. The player will be able to move around the map and interact with objects.
+Console.WriteLine("Hello, adventurer!");
+Thread.Sleep(500);
+Console.WriteLine("What is your name?");
+
+var regex = new Regex(@"^[a-zA-Z]+$");
+
+var playerName = Console.ReadLine();
+
+var validName = playerName != null && regex.IsMatch(playerName);
+
+while (!validName)
+{
+    Console.WriteLine("That's not a name!");
+    playerName = Console.ReadLine();
+    validName = playerName != null && regex.IsMatch(playerName);
+}
+
+Console.WriteLine($"Ah.. welcome {playerName}!");
+Thread.Sleep(2000);
+
+System.Threading.Thread.Sleep(500);
+Console.WriteLine("Our adventure will start in...");
+for (int i = 10; i > 0; i--)
+{
+    Console.WriteLine($"{i} seconds");
+    System.Threading.Thread.Sleep(1000);
+    
+}
+
+Console.WriteLine("Let's go! :D");
+Console.WriteLine(@"
+You sit by the campfire, the flames dancing in the wind.
+        ______
+jgs    /     /\
+      /     /  \
+     /_____/----\_    (  
+    ""     ""          ).  
+   _ ___          o (:') o   
+  (@))_))        o ~/~~\~ o   
+                  o  o  o
+");
+
+Console.WriteLine("");
+
+
