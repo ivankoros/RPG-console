6960d86dac00b9396f44d3bdbd6cdd9ed02dc4ff
ivankoros <ivan.koros2@gmail.com>
2023-04-03 23:59:45 -0400

    added a NaturalWrite function to modularize the writing out of text. It pauses at ends of sentences and slightly less so at commas and semicolons to achieve a natural writing style

--- Program.cs
+++ Program.cs
@@ -1,9 +1,37 @@
 ﻿using System;
+using System.Net.Sockets;
 using System.Threading;
 using System.Text.RegularExpressions;
 
+static string NaturalPrint(string line)
+{
+    for (int i = 0; i < line.Length; i++)
+    {
+        switch (line[i])
+        {
+            case '.':
+            case '!':
+            case '?':
+                Console.Write(line[i]);
+                Thread.Sleep(500);
+                break;
+            case ',':
+            case ';':
+                Console.Write(line[i]);
+                Thread.Sleep(250);
+                break;
+            default:
+                Console.Write(line[i]);
+                Thread.Sleep(50);
+                break;
+        }
+    }
+
+    return line;
+}
+
 // This is an text-based adventure game. The player will be able to move around the map and interact with objects.
-Console.WriteLine("Hello, adventurer!");
+NaturalPrint("Hello, adventurer!");
 Thread.Sleep(500);
 Console.WriteLine("What is your name?");
 
@@ -24,7 +52,7 @@ Console.WriteLine($"Ah.. welcome {playerName}!");
 Thread.Sleep(2000);
 
 System.Threading.Thread.Sleep(500);
-Console.WriteLine("Our adventure is starting...");
+Console.Write("Our adventure is starting");
 for (int i = 0; i < 10; i++)
 {
     Thread.Sleep(500); // wait for half a second
@@ -48,28 +76,30 @@ Thread.Sleep(2000);
 var line = "You look around. Surrounded by a forest, neither the surrounding trees nor the log you're" +
            "sitting on seem familiar.";
 
-for (int i = 0; i < line.Length; i++)
-{
-    Console.Write(line[i]);
-    Thread.Sleep(50);
-}
+NaturalPrint(line);
+
 Console.WriteLine("");
 Thread.Sleep(2000);
 
 line = "Does it matter? The fire is warm, and you feel safe.";
 Console.ForegroundColor = ConsoleColor.Green;
-for (int i = 0; i < 10; i++)
+for (int i = 0; i < line.Length; i++)
 {
     Console.Write(line[i]);
 }
 Console.ResetColor();
 Console.WriteLine("");
-ad
+
 Thread.Sleep(2000);
-Console.WriteLine("'I guess not,' you say out loud. \n");
+line = "'I guess not,' you say out loud. \n";
+for (int i = 0; i < line.Length; i++)
+{
+    Console.Write(line[i]);
+    Thread.Sleep(50);
+}
 Thread.Sleep(2000);
 
-Console.WriteLine("Relaxing, you close your eyes and let the warmth of the fire calm you.");
+Console.WriteLine("Relaxing, you close your eyes and let the warmth of the fire calm you. \n");
 Thread.Sleep(1500);
 Console.WriteLine("You're exhausted, although you don't seem to remember why. \n");
 Thread.Sleep(1000);
