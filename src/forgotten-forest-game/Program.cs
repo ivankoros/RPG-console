fe88cca7a357a2a1fbac4babda170a05a1476b7d
ivankoros <ivan.koros2@gmail.com>
2023-04-17 23:56:07 -0400

    added namespace and main class components for better separation of functions

--- src/forgotten-forest-game/Program.cs
+++ src/forgotten-forest-game/Program.cs
@@ -1,69 +1,39 @@
-﻿using System;
-using System.Net.Sockets;
-using System.Reflection.Metadata;
-using System.Threading;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
-static string NaturalPrint(string line, int newLineDelay = 1000, int endDelay = 500)
+namespace RPG_console.forgotten_forest_game
 {
-    for (int i = 0; i < line.Length; i++)
+    public class Program
     {
-        switch (line[i])
+        public static void Main(string[] args)
         {
-            case '.':
-            case '!':
-            case '?':
-                Console.Write(line[i]);
-                Thread.Sleep(endDelay);
-                break;
-            case ',':
-            case ';':
-                Console.Write(line[i]);
-                Thread.Sleep(250);
-                break;
-            default:
-                Console.Write(line[i]);
-                Thread.Sleep(50);
-                break;
-        }
-    }
-
-    Console.WriteLine("");
-    Console.WriteLine("");
-
-    Thread.Sleep(newLineDelay);
-
-    return line;
-}
-
 // Intro
-NaturalPrint("Hello, adventurer!");
+            NaturalPrint("Hello, adventurer!");
 
-NaturalPrint("What is your name?");
+            NaturalPrint("What is your name?");
 
-var regex = new Regex(@"^[a-zA-Z]+$");
+            var regex = new Regex(@"^[a-zA-Z]+$");
 
-var playerName = Console.ReadLine();
+            var playerName = Console.ReadLine();
 
-var validName = playerName != null && regex.IsMatch(playerName);
+            var validName = playerName != null && regex.IsMatch(playerName);
 
-while (!validName)
-{
-    Console.WriteLine("That's not a name!");
-    playerName = Console.ReadLine();
-    validName = playerName != null && regex.IsMatch(playerName);
-}
+            while (!validName)
+            {
+                Console.WriteLine("That's not a name!");
+                playerName = Console.ReadLine();
+                validName = playerName != null && regex.IsMatch(playerName);
+            }
 
-NaturalPrint($"Ah.. welcome {playerName}!",
-    2000);
+            NaturalPrint($"Ah.. welcome {playerName}!",
+                2000);
 
-NaturalPrint("Our adventure is starting.....");
+            NaturalPrint("Our adventure is starting.....");
 
 
-NaturalPrint("You're sitting by a campfire; the flames dance fiercely against a light wind.",
-    newLineDelay:0);
+            NaturalPrint("You're sitting by a campfire; the flames dance fiercely against a light wind.",
+                newLineDelay: 0);
 
-Console.WriteLine(@"
+            Console.WriteLine(@"
 
         ______
 jgs    /     /\
@@ -75,39 +45,42 @@ jgs    /     /\
                   o  o  o
 ");
 
-Thread.Sleep(2000);
+            Thread.Sleep(2000);
 
-var line = "You look around. Surrounded by a forest, neither the trees that encapsulate your campground nor the log you're" +
-           " sitting on seem familiar.";
+            var line =
+                "You look around. Surrounded by a forest, neither the trees that encapsulate your campground nor the log you're" +
+                " sitting on seem familiar.";
 
-NaturalPrint(line, 2000);
+            NaturalPrint(line, 2000);
 
-Console.ForegroundColor = ConsoleColor.Green;
-NaturalPrint("Does it matter? The fire is warm, and you feel safe.",
-    2000);
-Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            NaturalPrint("Does it matter? The fire is warm, and you feel safe.",
+                2000);
+            Console.ResetColor();
 
-var doItMatter = null as string;
-while (doItMatter == null)
-{
-    doItMatter = Console.ReadLine();
-}
-NaturalPrint("'I guess not,' you say out loud.",
-    2000);
+            var doItMatter = null as string;
+            while (doItMatter == null)
+            {
+                doItMatter = Console.ReadLine();
+            }
 
-NaturalPrint("Relaxing, you close your eyes and let the warmth of the fire calm you.",
-    1500);
+            NaturalPrint("'I guess not,' you say out loud.",
+                2000);
 
-NaturalPrint("You're exhausted, although you don't seem to remember why.");
+            NaturalPrint("Relaxing, you close your eyes and let the warmth of the fire calm you.",
+                1500);
 
-NaturalPrint("You realize you haven't eaten all day; you're starving." +
-             " When was the last time you ate?",
-    newLineDelay: 3000);
+            NaturalPrint("You're exhausted, although you don't seem to remember why.");
+
+            NaturalPrint("You realize you haven't eaten all day; you're starving." +
+                         " When was the last time you ate?",
+                newLineDelay: 3000);
 
 
-NaturalPrint("Hunger overcomes curiosity. You open your eyes and see a hiking sack on the ground to your right.",
-    0);
-Console.WriteLine(@"
+            NaturalPrint(
+                "Hunger overcomes curiosity. You open your eyes and see a hiking sack on the ground to your right.",
+                0);
+            Console.WriteLine(@"
 
        .--._ .
         \ ).'
@@ -124,48 +97,48 @@ Console.WriteLine(@"
 ''""'''""'''""'''""""'''
 ");
 
-Thread.Sleep(2200);
+            Thread.Sleep(2200);
 
-NaturalPrint("What will you do?");
-var campfireFirstResponse = null as string;
+            NaturalPrint("What will you do?");
+            var campfireFirstResponse = null as string;
 
-while (campfireFirstResponse == null)
-{
-    campfireFirstResponse = Console.ReadLine();
-}
+            while (campfireFirstResponse == null)
+            {
+                campfireFirstResponse = Console.ReadLine();
+            }
 
-campfireFirstResponse = Regex.Replace(campfireFirstResponse, @"[^\w]", "");
+            campfireFirstResponse = Regex.Replace(campfireFirstResponse, @"[^\w]", "");
 
-if (campfireFirstResponse!.Contains("look"))
-{
-    NaturalPrint("You decide to ruffle through the hiking sack.",
-        newLineDelay:0);
-        
-    NaturalPrint(".....");
+            if (campfireFirstResponse!.Contains("look"))
+            {
+                NaturalPrint("You decide to ruffle through the hiking sack.",
+                    newLineDelay: 0);
 
-    Console.Clear();
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine("You should've just gone to sleep.");
-    Console.ResetColor();
+                NaturalPrint(".....");
 
-    Console.WriteLine();
-}
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You should've just gone to sleep.");
+                Console.ResetColor();
 
-NaturalPrint(@"""Restart"" ?");
+                Console.WriteLine();
+            }
 
-var restart = null as string;
+            NaturalPrint(@"""Restart"" ?");
 
-while (restart == null)
-{
-    restart = Console.ReadLine();
-}
+            var restart = null as string;
 
-if (restart.Contains('y'))
-{
-    Console.Clear();
+            while (restart == null)
+            {
+                restart = Console.ReadLine();
+            }
 
-    NaturalPrint("You're sitting by a campfire; your mind feels uneasy as the flame roars.");
-    Console.WriteLine(@"
+            if (restart.Contains('y'))
+            {
+                Console.Clear();
+
+                NaturalPrint("You're sitting by a campfire; your mind feels uneasy as the flame roars.");
+                Console.WriteLine(@"
 
 
         ______
@@ -178,7 +151,41 @@ jgs    /     /\
                   o  o  o
 ");
 
-    NaturalPrint("The fire feels familiar.");
-    Thread.Sleep(20000);
-}
+                NaturalPrint("The fire feels familiar.");
+                Thread.Sleep(20000);
+            }
+        }
 
+        private static string NaturalPrint(string line, int newLineDelay = 1000, int endDelay = 500)
+        {
+            foreach (var word in line)
+            {
+                switch (word)
+                {
+                    case '.':
+                    case '!':
+                    case '?':
+                        Console.Write(word);
+                        Thread.Sleep(endDelay);
+                        break;
+                    case ',':
+                    case ';':
+                        Console.Write(word);
+                        Thread.Sleep(250);
+                        break;
+                    default:
+                        Console.Write(word);
+                        Thread.Sleep(50);
+                        break;
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Thread.Sleep(newLineDelay);
+
+            return line;
+        }
+    }
+}
